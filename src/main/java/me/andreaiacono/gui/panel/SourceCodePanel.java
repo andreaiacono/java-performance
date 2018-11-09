f0c9cdcc0bcf194bec1ee925d1048cc5f0e35ddf
Andrea Iacono <aiacono@ebay.com>
2018-11-09 10:49:10 +0100

    added dark theme to source code editor

--- src/main/java/me/andreaiacono/gui/panel/SourceCodePanel.java
+++ src/main/java/me/andreaiacono/gui/panel/SourceCodePanel.java
@@ -3,6 +3,7 @@ package me.andreaiacono.gui.panel;
 import me.andreaiacono.utils.FileUtils;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rsyntaxtextarea.Theme;
 import org.fife.ui.rtextarea.RTextScrollPane;
 
 import javax.swing.*;
@@ -19,6 +20,13 @@ public class SourceCodePanel extends JPanel {
         textArea = new RSyntaxTextArea();
         textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
         textArea.setCodeFoldingEnabled(true);
+
+        try {
+            Theme theme = Theme.load(getClass().getResourceAsStream("/rsyntaxtheme_dark.xml"));
+            theme.apply(textArea);
+        }
+        catch (Exception ignored) { }
+
         RTextScrollPane scrollPane = new RTextScrollPane(textArea);
         add(scrollPane);
 

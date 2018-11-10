1876d33f726078e267867662aa877d299307c471
Andrea Iacono <aiacono@ebay.com>
2018-11-10 09:32:28 +0100

    removed dark theme

--- src/main/java/me/andreaiacono/gui/panel/SourceCodePanel.java
+++ src/main/java/me/andreaiacono/gui/panel/SourceCodePanel.java
@@ -3,7 +3,6 @@ package me.andreaiacono.gui.panel;
 import me.andreaiacono.utils.FileUtils;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
-import org.fife.ui.rsyntaxtextarea.Theme;
 import org.fife.ui.rtextarea.RTextScrollPane;
 
 import javax.swing.*;
@@ -20,12 +19,6 @@ public class SourceCodePanel extends JPanel {
         textArea = new RSyntaxTextArea();
         textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
         textArea.setCodeFoldingEnabled(true);
-
-        try {
-            Theme theme = Theme.load(getClass().getResourceAsStream("/rsyntaxtheme_dark.xml"));
-            theme.apply(textArea);
-        }
-        catch (Exception ignored) { }
 
         RTextScrollPane scrollPane = new RTextScrollPane(textArea);
         add(scrollPane);
